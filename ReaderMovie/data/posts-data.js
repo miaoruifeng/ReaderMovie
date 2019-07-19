33218aeadb83fb73a6d4696166f9d366868461f1
miaoruifeng <mrfweb@163.com>
2019-07-19 18:13:09 +0800

    'first'

---
+++ ReaderMovie/data/posts-data.js
@@ -0,0 +1,59 @@
+var local_bannerurls = [
+    "/images/swiper/swiper1.png",
+    "/images/swiper/swiper2.png",
+    "/images/swiper/swiper3.png",
+]
+var local_database = [{
+    date: "May 10 2019",
+    title: "正是虾肥蟹壮时",
+    avatar: "/images/avatar/0.png",
+    cover: "/images/post/crab.png",
+    content: "菊黄蟹正肥，品尝秋之味。徐志摩把“看初花的荻芦”和“到楼外楼吃蟹”,并列为秋天来杭州不能错过的风雅之事；用林妹妹的话讲是“螯封嫩玉双双满” ",
+    collection: "96",
+    view: "112"
+}, {
+    date: "May 25 2019",
+    title: "比利·林恩的中场故事",
+    avatar: "/images/avatar/1.png",
+    cover: "/images/post/bl.png",
+    content: "李安是一位绝不会重复自己的导演，本片将极富原创性李安众所瞩目的新片《比利林恩漫长的中场休息》，正式更名《半场无战事》",
+    collection: "50",
+    view: "180"
+}, {
+    date: "May 25 2019",
+    title: "当我们在谈论经济学时，我们在谈论什么?",
+    avatar: "/images/avatar/2.png",
+    cover: "/images/post/sls.jpg",
+    content: "引言在我跟学生课后交流时，以及我在知乎上阅读有关“经济”问题的论题时，经常会遇到这样的情况：...",
+    collection: "84",
+    view: "68"
+}, {
+    date: "May 25 2019",
+    title: "微信·小程序开发工具安装指南",
+    avatar: "/images/avatar/3.png",
+    cover: "/images/post/xiaolong.jpg",
+    content: "这两天闲来无事，也安装了 “微信折叠”的开发工具来玩一下。以下是一些小道消息及使用体验，过两天我会写一篇文章以开发者的角度来详细评价微信小程序",
+    collection: "50",
+    view: "180"
+}, {
+    date: "May 25 2019",
+    title: "从视觉到触觉 这款VR手套能给你真实触感",
+    avatar: "/images/avatar/4.png",
+    cover: "/images/post/vr.png",
+    content: "8月29日消息，据国外媒体VentureBeat报道，一家名为Dexta Robotics的公司最近发布了一款有望变革虚拟现实手部追踪与交互方式的新产品",
+    collection: "50",
+    view: "180"
+}, {
+    date: "May 25 2019",
+    title: "爱奇艺创维开展战略合作，合力布局开放娱乐生态",
+    avatar: "/images/avatar/5.png",
+    cover: "/images/post/iqiyi.png",
+    content: "爱奇艺和创维分别作为国内领先的在线视频品牌和家电品牌",
+    collection: "50",
+    view: "180"
+}]
+
+module.exports = {
+    postList: local_database,
+    bannerImgUrls: local_bannerurls
+}
